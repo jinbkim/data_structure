fd541ec2233c3d08f9bbc7a4db6cfdaa79e37d64
root <root@DESKTOP-Q20C1FA.localdomain>
2020-06-17 20:44:47 +0900

    20200617

--- 1_data_structure/6_sorting/quick_sort.c
+++ 1_data_structure/6_sorting/quick_sort.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	arr_swap(int *a, int *b)
+void	show_arr(int *arr, int size)
 {
-	int	temp;
+	int i ;
 	
-	temp = *a;
-	*a = *b;
-	*b = temp;
+	i = -1;
+	while(++i < size)
+		printf("%d ", arr[i]);
+	printf("\n");
 }
 
 void	ft_swap(int *a, int *b)
@@ -21,15 +22,15 @@ void	ft_swap(int *a, int *b)
 
 int		median_pivot(int *arr, int left, int right)
 {
-	int	example[] = {left, (left + right) / 2, right};
+	int	ex[] = {left, (left + right) / 2, right};
 	
-	if (arr[example[1]] < arr[example[0]])
-		ft_swap(&example[1], &example[0]);
-	if (arr[example[2]] < arr[example[1]])
-		ft_swap(&example[2], &example[1]);
-	if (arr[example[1]] < arr[example[0]])
-		ft_swap(&example[1], &example[0]);
-	return (example[1]);
+	if (arr[ex[1]] < arr[ex[0]])
+		ft_swap(&ex[1], &ex[0]);
+	if (arr[ex[2]] < arr[ex[1]])
+		ft_swap(&ex[2], &ex[1]);
+	if (arr[ex[1]] < arr[ex[0]])
+		ft_swap(&ex[1], &ex[0]);
+	return (ex[1]);
 }
 
 int		partition(int *arr, int left, int right)
@@ -38,11 +39,15 @@ int		partition(int *arr, int left, int right)
 	int	pivot;
 	int low;
 	int high;
-
-	pivot_idx = median_pivot(arr, left, right);  // store median index
+	
+	// estimate index of element with median
+	// that index is pivot index
+	pivot_idx = median_pivot(arr, left, right);
 	pivot = arr[pivot_idx];
+	
 	low = left + 1;
 	high = right;
+	
 	ft_swap(&arr[pivot_idx], &arr[left]);  // move pivot to left
 	
 	while (low <= high)
@@ -58,6 +63,7 @@ int		partition(int *arr, int left, int right)
 		if (low <= high)  // change location if not crossed
 			ft_swap(&arr[low], &arr[high]);	
 	}
+	
 	ft_swap(&arr[left], &arr[high]);
 	return (high);
 }
@@ -76,23 +82,14 @@ void	quick_sort(int *arr, int left, int right)
 	}
 }
 
-void	show_arr(int *arr, int size)
-{
-	int i ;
-	
-	i = -1;
-	while(++i < size)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
-
 
 
 // 411page. sorting
 int		main(void)
 {
-	int arr[] = {3, 2, 4, 1};
-
+	int arr[] = {5, 4, 3, 2, 1};
+	//int	arr[] = {3, 3, 3};
+	
 	show_arr(arr, sizeof(arr) / sizeof(int));
 	quick_sort(arr, 0, sizeof(arr) / sizeof(int) - 1);
 	show_arr(arr, sizeof(arr) / sizeof(int));
