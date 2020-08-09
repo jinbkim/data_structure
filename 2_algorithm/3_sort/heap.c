d371b02333d5843d445d487c879b1a1b143a0567
System Administrator <root@tests-iMac.local>
2020-08-09 20:56:53 +0900

    heap revise

--- 2_algorithm/3_sort/heap.c
+++ 2_algorithm/3_sort/heap.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
+#include <limits.h>
 
-#define	PQ_SIZE	100
-
-typedef	int	pq_data;
-typedef	int	(*priority_comp)(pq_data, pq_data);
-
-typedef	struct		s_p_queue
-{
-	int				data_num;
-	pq_data			pq_arr[PQ_SIZE];	
-	priority_comp	f;
-}					t_p_queue;
+typedef	int	(*priority_comp)(int, int);
 
 
 
@@ -24,87 +15,115 @@ void	show_arr(int *arr, int size)
 	printf("\n");
 }
 
-int		comp_func(pq_data a, pq_data b)
+int		comp_func(int a, int b)
 {
 	return (a - b);
 }
 
-
-void	comp_put(int *arr, int node_n, priority_comp f)
+void	heap_put(int *arr, int idx, priority_comp f)
 {
-	int	v;
+	int	input;
 
-	v = arr[node_n];
-	arr[0] = 9999;
-	while (f(arr[node_n / 2], v) < 0)
+	input = arr[idx];
+	while (f(arr[idx / 2], input) < 0)
 	{
-		arr[node_n] = arr[node_n / 2];
-		node_n /= 2;
+		arr[idx] = arr[idx / 2];
+		idx /= 2;
 	}
-	arr[node_n] = v;
-}
-void	heap_put(int *arr, int *node_n, int v, priority_comp f)
-{
-	arr[++(*node_n)] = v;
-	comp_put(arr, *node_n, f);
+	arr[idx] = input;
+
 }
 
-void	comp_get(int *arr, int node_n, priority_comp f)
-{
-	int v;
-	int	i;
-	int	j;
+// void	comp_get(int *arr, int i, priority_comp f)
+// {
+// 	int v;
+// 	int	ii;
+// 	int	j;
 
-	v = arr[1];
-	i = 1;
-	while (i <= node_n / 2)
+// 	v = arr[1];
+// 	ii = 1;
+// 	while (ii <= i / 2)
+// 	{
+// 		j = ii * 2;
+// 		if (j < i && arr[j] < arr[j + 1])
+// 			j++;
+// 		if (f(arr[j], v) < 0)
+// 			break ;
+// 		arr[ii] = arr[j];
+// 		ii = j; 
+// 	}
+// 	arr[ii] = v;
+// }
+
+int		heap_get(int *arr, int i, priority_comp f)
+{
+	int last_data;
+	int	output;
+	int	parent_i;
+	int	child_i;
+
+	printf("----- show0 -----\n");
+	show_arr(arr, 8);
+	output = arr[1];
+	// printf("--------------------\n");
+	// printf("arr[1] : %d, i : %d\n", arr[1], i);
+	arr[1] = arr[(i)];
+	// printf("arr[1] : %d, i : %d\n", arr[1], i);
+	// comp_get(arr, i, f);
+	last_data = arr[1];
+	// printf("last_data : %d\n", last_data);
+	parent_i = 1;
+	// printf("i : %d\n", i);
+	printf("----- show1 -----\n");
+	show_arr(arr, 8);
+	while (parent_i <= i / 2)
 	{
-		j = i * 2;
-		if (j < node_n && arr[j] < arr[j + 1])
-			j++;
-		if (f(arr[j], v) < 0)
+		child_i = parent_i * 2;
+		printf("i : %d, child_i : %d, arr[child_i] : %d, arr[child_i + 1] : %d\n", i, child_i, arr[child_i], arr[child_i + 1]);
+		if (child_i < i && (f(arr[child_i], arr[child_i + 1]) < 0))
+			child_i++;\
+		if (f(arr[child_i], last_data) < 0)
 			break ;
-		arr[i] = arr[j];
-		i = j; 
+		arr[parent_i] = arr[child_i];
+		parent_i = child_i;
+		printf("----- show2 -----\n");
+		show_arr(arr, 8);
 	}
-	arr[i] = v;
-}
 
-int		heap_get(int *arr, int *node_n, priority_comp f)
-{
-	int	v;
-	
-	v = arr[1];
-	arr[1] = arr[(*node_n)--];
-	comp_get(arr, *node_n, f);
-	return (v);
+	arr[parent_i] = last_data;
+	printf("----- show3 -----\n");
+	show_arr(arr, 8);
+	printf("i : %d, child_i : %d, arr[child_i] : %d, arr[child_i + 1] : %d\n", i, child_i, arr[child_i], arr[child_i + 1]);
+	return (output);
 }
 
 void	heap_sort(int *arr, int n, priority_comp f)
 {
-	int	node_n;
 	int	i;
+	// int	i;
 
-	node_n = 0;
 	i = 0;
+	// i = 0;
 	while (++i <= n)
-		heap_put(arr, &node_n, arr[i], f);
-	
-	printf("node_n : %d\n", node_n);
-	i = node_n + 1;
-	while (1 <= --i)
 	{
-		printf("arr[%d] : %d\n", i, arr[i] = heap_get(arr, &node_n, f));
+		heap_put(arr, i, f);
+
+		// heap_put(arr, ++i, f);
+		// printf("i : %d, i : %d\n", i , i);
 	}
+	// i = i + 1;
+	// i = (--i) + 1;
+	// i++;
+	// printf("i : %d, i: %d\n", i , i);
+	while (0 < --i)
+		arr[i] = heap_get(arr, i, f);
 }
-
-
 
 
 
 int		main(void)
 {
-	int arr[8] = {9999, 6, 3, 1, 7, 4, 2, 5};
+	int arr[] = {INT_MAX, 3, 2, 1, 99, 88, 77};
 
 	show_arr(arr, sizeof(arr) / sizeof(int));
 	heap_sort(arr, sizeof(arr) / sizeof(int) - 1, comp_func);
