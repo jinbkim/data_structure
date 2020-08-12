1e2c374c98ae4b7b5292c1f4e6e68c9de389b2af
42Jiphyeonjeon <sejong_the_great@42Jiphyeonjeonui-iMac.local>
2020-08-12 17:23:38 +0900

    recursive revise

--- 2_algorithm/2_data_structure/recursive/draw_circle.c
+++ 2_algorithm/2_data_structure/recursive/draw_circle.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <mlx.h>
+#include <math.h>
 
 # define WIND_W 1000
 # define WIND_H	1000
@@ -20,19 +21,17 @@ typedef	struct	s_mlx
 
 
 
-void	draw_line(t_mlx *mlx, int x1, int y1, int x2, int y2, int color)
+void	draw_cirlce(t_mlx *mlx, int x, int y, int r, int color)
 {
-	if (-1 <= x1 - x2 && x1 - x2 <= 1 && -1 <= y1 - y2 && y1 - y2 <= 1)
-		return ;
-	else
+	int i;
+
+	i = 0;
+	while (i <= M_PI * 2)
 	{
-		mlx->i_dat[(y1 + y2) / 2 * IMAG_H + (x1 + x2) / 2] = color;
-		draw_line(mlx, x1, y1, (x1 + x2) / 2, (y1 + y2) / 2, color);
-		draw_line(mlx, x2, y2, (x1 + x2) / 2, (y1 + y2) / 2, color);
+		mlx->i_dat[y * IMAG_H + x] = color;
+		i += 0.01;
 	}
 }
-
-
 
 int		main(void)
 {
@@ -43,7 +42,7 @@ int		main(void)
 	mlx.imag = mlx_new_image(mlx.mlx, IMAG_W, IMAG_H);
 	mlx.i_dat = (int *)mlx_get_data_addr(mlx.imag, &mlx.bpp, &mlx.size_l, &mlx.endi);
 
-	draw_line(&mlx, 100, 100, 300, 600, 0xff0000);
+	draw_circle(&mlx, 500, 500, 100, 0xff0000);
 
 	mlx_put_image_to_window(mlx.mlx, mlx.wind, mlx.imag, 0 , 0);
 	mlx_loop(mlx.mlx);
